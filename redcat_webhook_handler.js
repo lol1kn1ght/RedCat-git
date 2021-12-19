de79f1a3ab2217c2df26ffd57f211fc0d186c207
DriZ <mygassanya@gmail.com>
2021-12-19 20:44:04 +0300

    changed filename

---
+++ redcat_webhook_handler.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const createHandler = require("github-webhook-handler");
+const shell = require("shelljs");
+const REPO_NAME = "RedCat-git";
+const PORT = process.env.PORT || 6769;
+
+let handler = createHandler({path: "/", secret: "redcattest"});
+
+http
+  .createServer((req, res) => {
+    handler(req, res, () => {
+      res.statusCode = 404;
+      res.end("no such location");
+    });
+  })
+  .listen(PORT);
+
+handler.on("error", err => console.error("Error:", err.message));
+handler.on("push", function(event) {
+  const repository = event.payload.repository.name;
+  console.log(`Получен Push из ${repository}`);
+  if (repository === REPO_NAME) {
+    shell.cd("..");
+    shell.exec("~/bots/RedCat-git/auto_deploy");
+  }
+});
