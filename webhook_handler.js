37fb487a3666a8c14775a7d8a167326942b9dba1
DriZ <mygassanya@gmail.com>
2021-12-19 20:34:12 +0300

    config changes

--- webhook_handler.js
+++ webhook_handler.js
@@ -2,10 +2,11 @@ const http = require("http");
 const createHandler = require("github-webhook-handler");
 const shell = require("shelljs");
 const REPO_NAME = "RedCat-git";
-const PORT = process.env.PORT || 6768;
+const PORT = process.env.PORT || 6769;
 
-let handler = createHandler({path: "/", secret: "AUTOUPDATE"});
-
+console.log("hui1")
+let handler = createHandler({path: "/", secret: "redcattest"});
+console.log("hui2")
 http
   .createServer((req, res) => {
     handler(req, res, () => {
@@ -15,12 +16,13 @@ http
   })
   .listen(PORT);
 
+console.log("hui3")
 handler.on("error", err => console.error("Error:", err.message));
 handler.on("push", function(event) {
   const repository = event.payload.repository.name;
   console.log(`Получен Push из ${repository}`);
   if (repository === REPO_NAME) {
     shell.cd("..");
-    shell.exec("~/bots/RedCat/auto_deploy");
+    shell.exec("~/bots/RedCat-git/auto_deploy");
   }
 });
