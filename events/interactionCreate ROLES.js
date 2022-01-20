34f887fa3f5155eb363dcebb6f17446439e7b528
loli-knight <60938098+KnightDark-loli@users.noreply.github.com>
2022-01-20 18:51:41 +0300

    исправлена ошибка с проверкой и дополнены ответы

--- events/interactionCreate ROLES.js
+++ events/interactionCreate ROLES.js
@@ -4,7 +4,7 @@ class Event {
   async execute(bot, mongo, interaction) {
     if (!interaction.isButton()) return;
 
-    if (!interaction.channel.id != "652521261058228236") return;
+    if (interaction.channel.id != "652521261058228236") return;
 
     let role = interaction.guild.roles.cache.get(interaction.customId);
 
@@ -12,14 +12,19 @@ class Event {
 
     let interaction_member = interaction.member;
 
-    interaction.reply({
-      content: `Вам выдана роль ${role}`,
-      ephemeral: true
-    });
-
     if (interaction_member.roles.cache.has(role.id)) {
+      interaction.reply({
+        content: `Снял вам роль ${role}`,
+        ephemeral: true
+      });
+
       return interaction_member.roles.remove(role.id);
     }
+
+    interaction.reply({
+      content: `Выдал вам роль ${role}`,
+      ephemeral: true
+    });
 
     interaction_member.roles.add(role.id);
   }
