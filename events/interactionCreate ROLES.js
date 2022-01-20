1cb47cb6ee4b6b62eeaf0e52ccea9667fd1123a8
loli-knight <60938098+KnightDark-loli@users.noreply.github.com>
2022-01-20 18:21:48 +0300

    новый евент для выдачи роли по кнопкам

---
+++ events/interactionCreate ROLES.js
@@ -0,0 +1,29 @@
+class Event {
+  constructor() {}
+
+  async execute(bot, mongo, interaction) {
+    if (!interaction.isButton()) return;
+
+    if (!interaction.channel.id != "652521261058228236") return;
+
+    let role = interaction.guild.roles.cache.get(interaction.customID);
+
+    if (!role) return;
+
+    let interaction_member = interaction.member;
+
+    interaction.reply({
+      content: `Вам выдана роль ${role}`,
+      ephemeral: true
+    });
+
+    if (interaction_member.roles.cache.has(role.id)) {
+      return interaction_member.roles.remove(role.id);
+    }
+
+    interaction_member.roles.add(role.id);
+  }
+}
+module.exports = (...args) => {
+  new Event().execute(...args);
+};
