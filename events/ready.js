611912fd41a39774046252a999629d5ad8271406
sergio971st <sergio971st@gmail.com>
2022-02-02 00:38:53 +0300

    changed bot's status

--- events/ready.js
+++ events/ready.js
@@ -5,8 +5,8 @@ class Event {
     let logs_channel = bot.channels.cache.get("802191792820191262");
 
     bot.user.setPresence({
-      activities: [{name: "V.3 Уже здесь!"}],
-      status: "dnd"
+      activities: [{type: "WATCHING", name: "#SaveRedDeadOnline"}],
+      status: "idle"
     });
 
     if (!logs_channel) return;
