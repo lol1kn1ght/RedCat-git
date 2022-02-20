83d027d3308f56e6e70f474cc6129d6ae358f3a6
Nakumanexe <89326701+Nakumanexe@users.noreply.github.com>
2022-02-20 11:37:14 +0700

    Update ready.js
    changed bot's status

--- events/ready.js
+++ events/ready.js
@@ -3,11 +3,14 @@ class Event {
 
   async execute(bot, mongo, f) {
     let logs_channel = bot.channels.cache.get("802191792820191262");
-
+  
     bot.user.setPresence({
-      activities: [{type: "WATCHING", name: "#SaveRedDeadOnline"}],
-      status: "idle"
-    });
+            status: 'idle',
+            activities: [{
+                name: '#SaveRedDeadOnline',
+                type: 'WATCHING'
+            }]
+        });
 
     if (!logs_channel) return;
 
