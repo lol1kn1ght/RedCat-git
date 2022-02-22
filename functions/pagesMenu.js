2f9ae07209e71853fd3b6aa7b2d3bf50e16d762a
Nakumanexe <89326701+Nakumanexe@users.noreply.github.com>
2022-02-22 12:22:24 +0700

    fix buttons

--- functions/pagesMenu.js
+++ functions/pagesMenu.js
@@ -54,7 +54,7 @@ module.exports = async function(message, pages, time, filter, start_page) {
         --menu_page;
 
         if (menu_page - 1 < 0) prev_page.disabled = true;
-        if (menu_page + 1 > pages.length - 1) next_page.disabled = false;
+        if (menu_page + 1 > 0) next_page.disabled = false;
         
        let new_row_1 = new Discord.MessageActionRow().addComponents(
           prev_page,
@@ -69,7 +69,7 @@ module.exports = async function(message, pages, time, filter, start_page) {
         ++menu_page;
         
         if (menu_page - 1 >= 0) prev_page.disabled = false;
-        if (menu_page + 1 > pages.length - 1) next_page.disabled = true;
+        if (menu_page + 1 <= 0) next_page.disabled = true;
 
          let new_row_2 = new Discord.MessageActionRow().addComponents(
           prev_page,
