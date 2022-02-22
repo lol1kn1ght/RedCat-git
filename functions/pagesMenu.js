449d06cc313ce23f60149397fd245cc09df57295
Nakumanexe <89326701+Nakumanexe@users.noreply.github.com>
2022-02-22 12:02:05 +0700

    fix или опять сломал

--- functions/pagesMenu.js
+++ functions/pagesMenu.js
@@ -66,7 +66,8 @@ module.exports = async function(message, pages, time, filter, start_page) {
         // Листнуть страницу вперед
         if (menu_page + 1 > pages.length - 1) return;
         ++menu_page;
-
+        
+        if (menu_page + 1 > pages.length - 1) prev_page.disabled = true;
         if (menu_page - 1 >= 0) prev_page.disabled = false;
 
          let new_row_2 = new Discord.MessageActionRow().addComponents(
