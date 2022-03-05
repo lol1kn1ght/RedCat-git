b3883732ee8d4428a0d742dc28b1bd5f833510ba
Nakumanexe <89326701+Nakumanexe@users.noreply.github.com>
2022-03-05 10:29:39 +0700

    fix статус бота

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,7 @@ class Event {
   async execute(bot, mongo, f) {
     let logs_channel = bot.channels.cache.get("802191792820191262");
   
+    function updateStatus() {
     bot.user.setPresence({
             status: 'idle',
             activities: [{
@@ -11,6 +12,11 @@ class Event {
                 type: 'WATCHING'
             }]
         });
+    }
+        let time_update = 1000 * 60 * 60 * 6; //раз в 6 часов поднимает статус
+
+        updateStatus()
+        setInterval(updateStatus, time_update)
 
     if (!logs_channel) return;
 
