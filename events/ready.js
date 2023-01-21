cfe4cb26f4fb6b829a3152e7b942687728ec3180
sergio971st <sergio971st@gmail.com>
2023-01-21 16:52:19 +0300

    уменьшено время апдейта статуса

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ class Event {
 
   async execute(bot, mongo, f) {
     let logs_channel = bot.channels.cache.get("802191792820191262");
-  
+
     function updateStatus() {
     bot.user.setPresence({
             status: 'idle',
@@ -13,7 +13,7 @@ class Event {
             }]
         });
     }
-        let time_update = 1000 * 60 * 60 * 6; //раз в 6 часов поднимает статус
+        let time_update = 1000 * 60 * 60; //раз в час поднимает статус
 
         updateStatus()
         setInterval(updateStatus, time_update)
